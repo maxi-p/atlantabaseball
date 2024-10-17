a9aa1c863da5bcf36fa881d685903f9a4f451691
Maksim Petrushin <petrushin.maksim001@gmail.com>
2024-10-17 19:19:23 -0400

    initial setup

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.set('port', PORT);
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  next();
+});
+
+// Set static folder
+app.use(express.static('frontend/build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log('Server listening on port ' + PORT);
+});
