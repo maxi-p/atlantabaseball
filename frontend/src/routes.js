8a279852fe1201a0b273dbb35cadb3ada6607259
Maksim Petrushin <petrushin.maksim001@gmail.com>
2024-10-19 20:04:37 -0400

    removed unreachable TestTable route

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,12 +2,10 @@ import React from 'react'
 
 // Base
 const Tables = React.lazy(() => import('./views/base/tables/Tables'))
-const TestTables = React.lazy(() => import('./views/base/tables/TestTable'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/tables', name: 'Tables', element: Tables },
-  { path: '/test-tables', name: 'TestTables', element: TestTables },
 ]
 
 export default routes
